44e140b006e249e37d46ff5a49d164c772e03789
nayan-kunwar <nayankunwar678@gmail.com>
2025-05-31 09:38:04 +0530

    Implement pagination for asset retrieval; add getAllAssets controller and pagination utility

--- src/controllers/asset/asset.controller.js
+++ src/controllers/asset/asset.controller.js
@@ -1,6 +1,7 @@
 import Asset from "../../models/asset/asset.model.js";
 import ApiError from "../../utils/error/ApiError.js";
 import { asyncHandler } from "../../utils/error/asyncHandler.js";
+import { paginate } from "../../utils/pagination.js";
 
 export const createAsset = asyncHandler(async (req, res, next) => {
   const asset = await Asset.create(req.body);
@@ -13,6 +14,35 @@ export const createAsset = asyncHandler(async (req, res, next) => {
     success: true,
     message: "Asset created successfully",
     data: asset,
+  });
+});
+
+export const getAllAssets = asyncHandler(async (req, res, next) => {
+  const { page = 1, limit = 10, asset_type } = req.query;
+
+  // const filter = {
+  //   ...(asset_type && {
+  //     asset_type: { $regex: `^${asset_type}$`, $options: "i" },
+  //   }),
+  // };
+
+  const { data: assets, pagination } = await paginate(
+    Asset,
+    parseInt(page),
+    parseInt(limit)
+  );
+
+  if (!assets || assets.length === 0) {
+    return res
+      .status(200)
+      .json({ success: true, message: "No assets found", data: [] });
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Fetched all assets successfully",
+    pagination,
+    data: assets,
   });
 });
 
--- src/routes/asset/asset.routes.js
+++ src/routes/asset/asset.routes.js
@@ -2,11 +2,12 @@ import express from "express";
 import {
   createAsset,
   deleteAssetById,
+  getAllAssets,
   updateAssetById,
 } from "../../controllers/asset/asset.controller.js";
 
 const router = express.Router();
 
-router.route("/").post(createAsset);
+router.route("/").post(createAsset).get(getAllAssets);
 router.route("/:id").patch(updateAssetById).delete(deleteAssetById);
 export default router;
